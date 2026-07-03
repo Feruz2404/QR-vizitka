69c4fc499356e8cd9ea517e6a2ddef2f2fe434a0
Feruz <153690859+Feruz2404@users.noreply.github.com>
2026-07-03 14:18:35 +0500

    Update BrandedQrFrame.tsx

--- src/components/qr/BrandedQrFrame.tsx
+++ src/components/qr/BrandedQrFrame.tsx
@@ -20,6 +20,7 @@ export function BrandedQrFrame({
 			<div className="pointer-events-none absolute right-3 top-3 h-8 w-8 rounded-tr-[18px] border-r-2 border-t-2 border-blue-500/80" />
 			<div className="pointer-events-none absolute bottom-3 left-3 h-8 w-8 rounded-bl-[18px] border-b-2 border-l-2 border-blue-500/80" />
 			<div className="pointer-events-none absolute bottom-3 right-3 h-8 w-8 rounded-br-[18px] border-b-2 border-r-2 border-blue-500/80" />
+
 			<div className="relative rounded-[22px] bg-white p-2">{children}</div>
 		</div>
 	)
